efbfb6727abf442a75b2de256c450f2352f12066
sumitssk <sumit.kawade20@pccoepune.org>
2024-02-22 23:55:49 +0530

    add experimental file

---
+++ stock_buy_and_sell1.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cout<<"Enter the no of test cases:";
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cout<<"Enter the no of el in the list:";
+        cin>>n;
+        vector<int> arr;
+        for(int i=0;i<n;i++)
+        {
+            int temp;
+            cin>>temp;
+            arr.push_back(temp);
+        }
+
+    }
+    return 0;
+}
